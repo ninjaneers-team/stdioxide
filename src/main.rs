cbbfb80c35b7861cc98cbf18f1374f9b34c11d42
Michael Gerhold <michael.gerhold@ninjaneers.de>
2026-04-20 14:52:38 +0200

    refactor: extract logic into library crate

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,6 @@
 
-mod app;
-mod args;
-mod child;
-mod control;
-mod output;
-mod servers;
-
 use clap::Parser;
-
-use crate::args::Args;
+use stdioxide::{app, args::Args};
 
 fn main() -> Result<(), anyhow::Error> {
     let args = Args::parse();
